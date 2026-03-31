c83b31ab6ca2524398bed774ce85b173f8c540a1
danielrosadasilva <daniel_9153@hotmail.com>
2026-03-31 00:34:32 -0300

    feat: created modal and table in users

--- Dto/User/CreateUserDto.cs
+++ Dto/User/CreateUserDto.cs
@@ -2,11 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace suprimmil.Dto.Access;
 
-public class LoginDto
+public class CreateUserDto
 {
     [Required(ErrorMessage = "O email é obrigatório.")]
     [EmailAddress(ErrorMessage = "O email informado é inválido.")]
     public string Email { get; set; } = string.Empty;
     [Required(ErrorMessage = "A senha é obrigatória.")]
     public string Password { get; set; } = string.Empty;
+
+    public bool IsAdmin { get; set; } = false;
 }
